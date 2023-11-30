4363b7a4e2a2bff4cd675dcadbce6fbf9f63790d
Adity012 <149262704+Adity012@users.noreply.github.com>
2023-11-30 09:27:10 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,4 +1,4 @@
 api_id = "26368249"
 api_hash = "ac2db262d5eb7ce0efc0162404c8d172"
-bot_token = "6405034289:AAElwVFStmMbFg8mWiREdGf0Qfq2nfQVUSg" 
+bot_token = "6713545950:AAG5Y-f4UDaVXeLnB1b1PV_NRW_OwaIk6m4" 
 auth_users = "5684410709"
